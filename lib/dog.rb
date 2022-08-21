8d17d020be7a40f5423ef68245696055a4673e89
+ <+>
2022-08-21 08:10:09 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
 class Dog
     def name=(dog_name)
-        @this_dogs_name = dog_name
+        @this_dogs_name = 'Lassie'
       end
     
       def name
         @this_dogs_name
       end
 
+      lassie = Dog.new
+      lassie.name = "Lassie"
 
 end
